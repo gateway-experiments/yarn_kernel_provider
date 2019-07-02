f306c565fc21752e902f528e250a18c805066945
Kevin Bates <kbates4@gmail.com>
2019-07-02 10:34:29 -0700

    Changes necessary to leverage KernelSpecProvider

--- yarn_kernel_provider/provider.py
+++ yarn_kernel_provider/provider.py
@@ -6,7 +6,7 @@ from remote_kernel_provider.provider import RemoteKernelProviderBase
 class YarnKernelProvider(RemoteKernelProviderBase):
 
     id = 'yarn'
-    kernels_dir = 'yarn_kernels'
+    kernel_file  = 'yarn_kernel.json'
     expected_process_class = 'yarn_kernel_provider.yarn.YarnClusterProcessProxy'
     supported_process_classes = [
         'enterprise_gateway.services.processproxies.yarn.YarnClusterProcessProxy',
